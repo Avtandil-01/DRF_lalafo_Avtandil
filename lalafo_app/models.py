7bf109d6f0a1653c2bae4d0f0723a1b86db8f143
tupoiaziat <satybaldievrenat@gmail.com>
2026-01-22 12:47:12 +0600

    DRF_1_Avtandil

---
+++ lalafo_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=32, unique=True)
+    category_image = models.ImageField()
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=32)
+    owner = models.CharField(max_length=32)
+    phone_number = PhoneNumberField(region='KG', blank=True, null=True)
+    description = models.TextField()
+    address = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    product_image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ImageProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField()
